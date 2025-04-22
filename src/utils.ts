9e3b819012edabdf85f6ceb4fe8dc28411404a46
Janice <github.snugly121@passmail.net>
2025-04-22 14:41:54 +0800

    Cleanup

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-export function getRandomBoundedCoord(
-  min: number,
-  max: number,
-  offset?: number,
-) {
-  const coord = (offset || 0) + min + Math.random() * (max - min);
-  return coord;
-}
