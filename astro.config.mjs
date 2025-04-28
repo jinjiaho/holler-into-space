4f0dafcbb01fb0b13fea6c8f60780f12ea98be3e
Janice <github.snugly121@passmail.net>
2025-04-28 14:13:44 +0800

    Add CSP the Astro way

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
+    },
+  },
+});
